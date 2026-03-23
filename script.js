0e224029aa7a90a683b927c8cb73cc8ea3859116
ann2007-alx <saniyaa51015@gmail.com>
2026-03-23 17:29:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById(pageId).classList.add("active");
 
 // SUPABASE
 const supabaseUrl = "https://lzuzuvseqonsettitnyo.supabase.co";
-const supabaseKey = "YOUR_KEY"; // paste your key here
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6dXp1dnNlcW9uc2V0dGl0bnlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NTU0MTAsImV4cCI6MjA4OTEzMTQxMH0.MRpz9Xv5vaqdEfjWqYa6rfnG0kwMX_5-1_sSB4vV1bc";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function sendFeedback(event){
