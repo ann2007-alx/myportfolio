2169c0ae6b05dc2890838f5f948de4e3c24a1da3
ann2007-alx <saniyaa51015@gmail.com>
2026-03-24 01:24:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("script loaded");
 function showPage(page) {
   document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
   document.getElementById(page).classList.add('active');
