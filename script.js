aeb49907235a44173304c1f4dcbca0aa0bcee5e8
ann2007-alx <saniyaa51015@gmail.com>
2026-03-23 17:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-document.getElementById("feedbackForm").addEventListener("submit", async function(event){
+// PAGE SWITCH
+function showPage(pageId){
+document.querySelectorAll(".page").forEach(page=>{
+page.classList.remove("active");
+});
+document.getElementById(pageId).classList.add("active");
+}
 
+// SUPABASE
+const supabaseUrl = "https://lzuzuvseqonsettitnyo.supabase.co";
+const supabaseKey = "YOUR_KEY"; // paste your key here
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function sendFeedback(event){
 event.preventDefault();
 
 const name = document.getElementById("name").value;
 const email = document.getElementById("email").value;
 const feedback = document.getElementById("feedback").value;
 
-const res = await fetch("/contact", {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
-},
-body: JSON.stringify({ name, email, feedback })
-});
+const { error } = await supabaseClient
+.from("feedback")
+.insert([{ name, email, feedback }]);
 
-const data = await res.json();
-
-if(data.success){
-alert("Thank you! Your feedback has been submitted.");
-document.getElementById("feedbackForm").reset();
-}else{
+if(error){
 alert("Error saving feedback");
+console.log(error);
+}else{
+alert("Thank you! Your feedback has been submitted.");
+document.querySelector("form").reset();
 }
-
-});
+}
